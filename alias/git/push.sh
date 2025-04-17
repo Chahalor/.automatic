fa0cc0ca3c98643daedd26cb1f60484f899350e9
nduvoid <nduvoid@student.42mulhouse.fr>
2025-04-17 17:17:47 +0200

    Auto commit

--- alias/git/push.sh
+++ alias/git/push.sh
@@ -4,6 +4,11 @@
 # ******Push the current branch to the remote repository***** #
 # *********************************************************** #
 
+
+if [ -f "Makefile" ]; then
+	make fclean
+fi
+
 git add .
 
 if [ -z "$1" ]; then
