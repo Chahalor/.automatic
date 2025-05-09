be5c510e0009617e78c95d94d24ab50fc9c5a64b
nduvoid <nathan@duvoid.fr>
2025-05-09 19:11:56 +0200

    alias/git: update pull.sh to init submodule

--- alias/git/pull.sh
+++ alias/git/pull.sh
@@ -3,5 +3,11 @@
 # This script pulls the latest changes from the remote repository and updates the local repository.
 # Usage: ./pull.sh [branch_name] || pull [branch_name]
 
-git pull --recurse-submodules $1
+# Pull du dépôt principal avec submodules
+git pull --recurse-submodules "$1"
+
+# Init + update récursif
+git submodule update --init --recursive
+
+# Optionnel : forcer les submodules à se mettre à jour sur leurs branches distantes
 git submodule update --recursive --remote
