d6bfad6bdeb98f5c6b5f4c257af2a27462325654
nduvoid <nduvoid@student.42mulhouse.fr>
2025-09-30 15:06:49 +0200

    alias/git/issue: add close option and rework open option

--- alias/git/issue.sh
+++ alias/git/issue.sh
@@ -3,23 +3,97 @@
 # *********************************************************** #
 # ****** create a new issue in the repository       ********* #
 # *********************************************************** #
-#  - Version: 0.0.1
-#  - Usage: ./issue.sh [issue title] [issue body]
+#  - Version: 2.0.0
+#  - Usage: ./issue.sh <action: open/close>
+#						- close <issue_number>
+#						- open <title> <content> <labels>
 
 # Exit if any command fails
 set -e
 
+function print_usage()
+{
+	echo "Usage: $0 <action: open/close>"
+	echo "  - open <title> <content> <labels>"
+	echo "  - close <issue_number>"
+	exit 1
+}
+
+function action_open()
+{
+	TITLE="$1"
+	CONTENT="$2"
+	LABEL="$3"
+
+	# Token GitHub depuis variable d'environnement
+	if [ -z "$GITHUB_TOKEN" ]; then
+		echo "Error: GITHUB_TOKEN is not set. Please set it in $AUTOMATIC_PATH/config.json: 'alias.git.issue.GITHUB_TOKEN'."
+		exit 1
+	fi
+
+	# Création de l'issue via API GitHub
+	curl -s -X POST \
+		-H "Authorization: token $GITHUB_TOKEN" \
+		-H "Accept: application/vnd.github.v3+json" \
+		"https://api.github.com/repos/$OWNER/$REPO/issues" \
+		-d "$(jq -n --arg t "$TITLE" --arg b "$CONTENT" --arg l "$LABEL" '{title:$t, body:$b, labels:[$l]}')"
+
+	echo "Issue '$TITLE' créée avec le label '$LABEL'."
+}
+
+function action_close()
+{
+	ISSUE_NUMBER="$1"
+
+	if [ -z "$GITHUB_TOKEN" ]; then
+		echo "Error: GITHUB_TOKEN is not set. Please set it in $AUTOMATIC_PATH/config.json: 'alias.git.issue.GITHUB_TOKEN'."
+		exit 1
+	fi
+
+	curl -s -X PATCH \
+		-H "Authorization: token $GITHUB_TOKEN" \
+		-H "Accept: application/vnd.github.v3+json" \
+		"https://api.github.com/repos/$OWNER/$REPO/issues/$ISSUE_NUMBER" \
+		-d '{
+			"state": "closed"
+		}'
+	echo "Issue #$ISSUE_NUMBER fermée."
+
+}
+
+ACTION="$1"
+
+case "$ACTION" in
+	"open")
+		;;
+	"close")
+		;;
+	*)
+		print_usage
+		;;
+esac
+
 # Vérification des arguments
-if [ $# -lt 3 ]; then
-	echo "Usage: $0 <flag> <title> <content>"
+if [ "$ACTION" == "open" ]; then
+	if [ $# -lt 4 ]; then
+		echo "Usage: $0 open <title> <content> <labels>"
+		exit 1
+	fi
+	TITLE="$2"
+	CONTENT="$3"
+	LABELS="$4"
+elif [ "$ACTION" == "close" ]; then
+	if [ $# -lt 2 ]; then
+		echo "Usage: $0 close <issue_number>"
+		exit 1
+	fi
+	ISSUE_NUMBER="$2"
+else
+	echo "Usage: $0 <action: open/close>"
 	exit 1
 fi
 
 GITHUB_TOKEN=$(jq -r '.alias.git.issue.GITHUB_TOKEN' "$AUTOMATIC_PATH/config.json")
-
-FLAG="$1"
-TITLE="$2"
-CONTENT="$3"
 
 # Récupérer l'URL du repo courant
 REPO_URL=$(git config --get remote.origin.url)
@@ -32,30 +106,9 @@ else
 	exit 1
 fi
 
-# Définir le label en fonction du flag
-case "$FLAG" in
-	-b|--bug)
-		LABEL="bug"
-		;;
-	-f|--feature)
-		LABEL="feature"
-		;;
-	*)
-		LABEL="general"
-		;;
-esac
-
-# Token GitHub depuis variable d'environnement
-if [ -z "$GITHUB_TOKEN" ]; then
-	echo "Error: GITHUB_TOKEN is not set. Please set it in $AUTOMATIC_PATH/config.json: 'alias.git.issue.GITHUB_TOKEN'."
-	exit 1
+# Exécuter l'action
+if [ "$ACTION" == "open" ]; then
+	action_open "$TITLE" "$CONTENT" "$LABELS"
+elif [ "$ACTION" == "close" ]; then
+	action_close "$ISSUE_NUMBER"
 fi
-
-# Création de l'issue via API GitHub
-curl -s -X POST \
-	-H "Authorization: token $GITHUB_TOKEN" \
-	-H "Accept: application/vnd.github.v3+json" \
-	"https://api.github.com/repos/$OWNER/$REPO/issues" \
-	-d "$(jq -n --arg t "$TITLE" --arg b "$CONTENT" --arg l "$LABEL" '{title:$t, body:$b, labels:[$l]}')"
-
-echo "Issue '$TITLE' créée avec le label '$LABEL'."
