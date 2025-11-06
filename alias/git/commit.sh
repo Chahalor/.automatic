10f000109b0581c34a90f366bccd31647120424f
nduvoid <nathan@duvoid.fr>
2025-11-06 16:22:31 +0100

    add(alias/git): new alias, commit

--- alias/git/commit.sh
+++ alias/git/commit.sh
@@ -16,7 +16,12 @@ if [ -z "${MESSAGE}" ]; then
 fi
 
 git status
-
+if git diff --quiet && git diff --cached --quiet && [[ -z $(git ls-files --others --exclude-standard) ]] && git log origin/$(git rev-parse --abbrev-ref HEAD)..HEAD --oneline | grep -q '^$'; then
+	echo "✅ no changes to commit"
+	exit 0
+else
+	read -p "Press [Enter] to continue or Ctrl+C to abort..."
+fi
 
 # Auto-clean build files if Makefile exists
 if [[ -f "Makefile" || -f "makefile" ]]; then
